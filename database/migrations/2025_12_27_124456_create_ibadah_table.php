457487177eac5750aa8df8642613ce95e08c6592
amrinrasyida <amrinrasyida@gmail.com>
2025-12-27 20:15:37 +0700

    first commit

---
+++ database/migrations/2025_12_27_124456_create_ibadah_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ibadah', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
+            $table->date('date');
+
+            $table->enum('kategori', [
+                'sholat_wajib',
+                'sholat_sunnah',
+                'puasa',
+                'dzikir',
+                'sedekah',
+                'lainnya'
+            ]);
+
+            $table->string('prayer_name');
+
+            $table->string('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ibadah');
+    }
+};
