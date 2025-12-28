2d4a62f574e232f639cb83453af3b23e8b3704e9
amrinrasyida <amrinrasyida@gmail.com>
2025-12-28 11:57:02 +0700

    mealplan

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,3 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate']);
-
-Route::get('/register', [LoginController::class, 'showRegister'])->name('register');
-Route::post('/register', [LoginController::class, 'register']);
