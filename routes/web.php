49b656e834e89ec2166dd08e65731d7f244fe619
AchmadDhafi <achmaddhafi2211@gmail.com>
2025-12-29 16:30:06 +0700

    ManajemenTugas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TugasController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [TugasController::class, 'index'])->name('tugas.index');
+
+Route::get('/tugas/cetak', [TugasController::class, 'exportPdf'])->name('tugas.export');
+
+Route::resource('tugas', TugasController::class)->except(['show']);
+
+Route::get('tugas/export', [TugasController::class, 'exportPdf'])->name('tugas.export');
